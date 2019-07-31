1251a745922e80303b9016cb7adf72036c4944ac
shinyay <shinya.com@gmail.com>
2019-07-31 21:40:38 +0900

    Modify id policy as auto

--- backend-app/src/main/kotlin/io/pivotal/shinyay/entity/Employee.kt
+++ backend-app/src/main/kotlin/io/pivotal/shinyay/entity/Employee.kt
@@ -2,13 +2,14 @@ package io.pivotal.shinyay.entity
 
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 data class Employee (
         @Id
-        @GeneratedValue
-        val id: String,
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val id: Long,
         val name: String,
         val role: String,
         val salary: Double
