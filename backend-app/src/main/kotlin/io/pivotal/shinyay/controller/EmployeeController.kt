535c2e2518e38e69c0c0e5858e41221313b620de
shinyay <shinya.com@gmail.com>
2019-07-31 21:57:54 +0900

    Add controller for employee with all lists

--- backend-app/src/main/kotlin/io/pivotal/shinyay/controller/EmployeeController.kt
+++ backend-app/src/main/kotlin/io/pivotal/shinyay/controller/EmployeeController.kt
@@ -1,9 +1,15 @@
 package io.pivotal.shinyay.controller
 
+import io.pivotal.shinyay.entity.Employee
+import io.pivotal.shinyay.repository.EmployeeRepository
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
-class EmployeeController {
+class EmployeeController(val employeeRepository: EmployeeRepository) {
+
+    @GetMapping(value = ["/employees", "/"])
+    fun employees(): MutableIterable<Employee> = employeeRepository.findAll()
 }
