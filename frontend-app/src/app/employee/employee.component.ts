11a95ebbc3b783115aa7c015e170cf1df9405620
shinyay <shinya.com@gmail.com>
2019-08-02 17:26:19 +0900

    Add controller component to access backend service

--- frontend-app/src/app/employee/employee.component.ts
+++ frontend-app/src/app/employee/employee.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClientService } from '../service/http-client.service';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
-
-  constructor() { }
-
+  employees:string[];
+  constructor(
+    private httpClientService:HttpClientService
+  ) { }
   ngOnInit() {
+    this.httpClientService.getEmployees().subscribe(
+     response =>this.handleSuccessfulResponse(response),
+    );
   }
-
+handleSuccessfulResponse(response)
+{
+    this.employees=response;
+}
 }
