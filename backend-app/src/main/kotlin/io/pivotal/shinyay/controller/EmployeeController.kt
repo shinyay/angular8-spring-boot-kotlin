07740cd94c3b6bd7092b41627d05f96b7ca657e3
shinyay <shinya.com@gmail.com>
2019-08-04 21:46:17 +0900

    Add Delete employee function

--- backend-app/src/main/kotlin/io/pivotal/shinyay/controller/EmployeeController.kt
+++ backend-app/src/main/kotlin/io/pivotal/shinyay/controller/EmployeeController.kt
@@ -2,6 +2,7 @@ package io.pivotal.shinyay.controller
 
 import io.pivotal.shinyay.entity.Employee
 import io.pivotal.shinyay.repository.EmployeeRepository
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -9,9 +10,20 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping(value = ["/employees", "/"])
 class EmployeeController(val employeeRepository: EmployeeRepository) {
 
-    @GetMapping(produces = ["application/json"])
+    @GetMapping(produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     fun employees(): MutableIterable<Employee> = employeeRepository.findAll()
 
     @PostMapping
     fun addEmployee(@RequestBody employee: Employee) = employeeRepository.save(employee)
+
+    @DeleteMapping(value = ["/{id}"])
+    fun deleteEmployee(@PathVariable(name = "id") id: String) = employeeRepository.deleteById(id.toLong())
+
+    @GetMapping(value = ["/init"])
+    fun initData() {
+        val employee1: Employee = Employee(1, "Foo", "Student", 100.0)
+        val employee2: Employee = Employee(2, "Bar", "Teacher", 1000.0)
+        employeeRepository.save(employee1)
+        employeeRepository.save(employee2)
+    }
 }
