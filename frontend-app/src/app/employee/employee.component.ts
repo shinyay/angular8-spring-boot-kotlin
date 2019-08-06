6fbb543d702c823692b4e8997f8c38e086678064
shinyay <shinya.com@gmail.com>
2019-08-07 07:49:33 +0900

    Add create employee logic

--- frontend-app/src/app/employee/employee.component.ts
+++ frontend-app/src/app/employee/employee.component.ts
@@ -8,28 +8,34 @@ import { HttpClientService, Employee } from '../service/http-client.service';
 })
 export class EmployeeComponent implements OnInit {
 
-  employees:Employee[];
-   
+  employees: Employee[];
+  employee: Employee = new Employee(0, "", "", 0)
+
   constructor(
-    private httpClientService:HttpClientService
+    private httpClientService: HttpClientService
   ) { }
 
   ngOnInit() {
     this.httpClientService.getEmployees().subscribe(
-     response =>this.handleSuccessfulResponse(response),
+      response => this.handleSuccessfulResponse(response),
     );
   }
 
-handleSuccessfulResponse(response)
-{
-    this.employees=response;
-}
+  handleSuccessfulResponse(response) {
+    this.employees = response;
+  }
 
-deleteEmployee(employee: Employee): void {
-  this.httpClientService.deleteEmployee(employee).subscribe( data => {
-//    this.employees = this.employees.filter((emp: Employee) => {emp !== employee});
-this.employees = this.employees.filter(emp => emp !== employee);
-  })
-}
+  deleteEmployee(employee: Employee): void {
+    this.httpClientService.deleteEmployee(employee).subscribe(data => {
+      //    this.employees = this.employees.filter((emp: Employee) => {emp !== employee});
+      this.employees = this.employees.filter(emp => emp !== employee);
+    })
+  }
+
+  createEmployee(): void {
+    this.httpClientService.createEmployee(this.employee).subscribe(data => {
+      alert("Employee created successfully.");
+    })
+  }
 
 }
