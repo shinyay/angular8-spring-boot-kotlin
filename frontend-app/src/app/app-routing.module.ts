b77433207a0d6d9331129c41600c54919f70b3e7
shinyay <shinya.com@gmail.com>
2019-08-08 11:20:12 +0900

    Add path to register new employee form

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EmployeeComponent} from './employee/employee.component'
+import { EmployeeComponent} from './employee/employee.component';
+import { NewEmployeeComponent } from './new-employee/new-employee.component';
 
 
 const routes: Routes = [
   { path:'', component: EmployeeComponent},
+  { path:'newemployee', component: NewEmployeeComponent},
 ];
 
 @NgModule({
