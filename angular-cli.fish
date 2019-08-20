01ca68a254906fa83f013f63f48a436a12c4ff52
shinyay <shinya.com@gmail.com>
2019-08-21 00:17:30 +0900

    Modify to enable entry directory path to change by option -d

--- angular-cli.fish
+++ angular-cli.fish
@@ -1,14 +1,18 @@
 #!/usr/bin/env fish
 
 function run_docker
-  argparse -n run_docker 'h/help' -- $argv
+  argparse -n run_docker 'h/help' 'd/directory=' -- $argv
   or return 1
 
   if set -lq _flag_help
     echo "angular-cli.fish <COMMAND>"
     return
   end
-  docker run --rm -it -v (pwd):/app shinyay/angular-cli $argv
+
+  set -lq _flag_directory
+  or set -l _flag_directory .
+
+  docker run --rm -it -v (pwd)/$_flag_directory:/app shinyay/angular-cli $argv
 end
 
 run_docker $argv
