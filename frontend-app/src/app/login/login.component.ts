fe1dd985d5413a3de2220ae77847363b88fe1461
shinyay <shinya.com@gmail.com>
2019-08-21 07:37:04 +0900

    Add Router component to change the new view

--- frontend-app/src/app/login/login.component.ts
+++ frontend-app/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
   selector: 'app-login',
