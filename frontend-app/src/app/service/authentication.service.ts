f1c84917de54e9ee932b09e0a585886c94d91f28
shinyay <shinya.com@gmail.com>
2019-08-20 22:29:20 +0900

    Add authentication and logout function

--- frontend-app/src/app/service/authentication.service.ts
+++ frontend-app/src/app/service/authentication.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class AuthenticationService {
 
   constructor() { }
+
+  authenticate(username, password) {
+    if (username === "angular" && password === "springboot") {
+      sessionStorage.setItem('username', username)
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('username')
+    console.log(!(user === null))
+    return !(user === null)
+  }
+
+  logOut() {
+    sessionStorage.removeItem('username')
+  }
 }
