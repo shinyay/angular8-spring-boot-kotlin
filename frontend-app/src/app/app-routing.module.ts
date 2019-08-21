bd541b8e0a8dde3345a90939bbe7d650e918339d
shinyay <shinya.com@gmail.com>
2019-08-21 23:26:41 +0900

    Add a routing for Login Component

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeComponent} from './employee/employee.component';
 import { NewEmployeeComponent } from './new-employee/new-employee.component';
-
+import { LoginComponent } from './login/login.component'
 
 const routes: Routes = [
   { path:'', component: EmployeeComponent},
   { path:'newemployee', component: NewEmployeeComponent},
+  { path:'login', component: LoginComponent },
 ];
 
 @NgModule({
