664cb65747700aa8f00f3f6e46a01a77691a7b5e
shinyay <shinya.com@gmail.com>
2019-08-21 23:22:02 +0900

    Modify initial value for login user name

--- frontend-app/src/app/login/login.component.ts
+++ frontend-app/src/app/login/login.component.ts
@@ -9,7 +9,7 @@ import { AuthenticationService } from '../service/authentication.service';
 })
 export class LoginComponent implements OnInit {
 
-  username = ''
+  username = 'angular'
   password = ''
   invalidLogin = false
 
