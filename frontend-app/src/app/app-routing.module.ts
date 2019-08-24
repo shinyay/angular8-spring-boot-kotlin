486faa5d44cd0009887dd8a644cdd395d70f3ecc
shinyay <shinya.com@gmail.com>
2019-08-24 10:34:17 +0900

    Add logout path

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeComponent} from './employee/employee.component';
 import { NewEmployeeComponent } from './new-employee/new-employee.component';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
   { path:'', component: EmployeeComponent},
   { path:'newemployee', component: NewEmployeeComponent},
   { path:'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent},
+
 ];
 
 @NgModule({
