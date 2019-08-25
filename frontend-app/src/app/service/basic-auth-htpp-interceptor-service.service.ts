4b85839debf227fc9bcef0b5f0498326a698f753
shinyay <shinya.com@gmail.com>
2019-08-25 21:25:38 +0900

    Add Interceptpr to add authorization header

--- frontend-app/src/app/service/basic-auth-htpp-interceptor-service.service.ts
+++ frontend-app/src/app/service/basic-auth-htpp-interceptor-service.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
-import {} from '@angular/common/http'
+import { HttpInterceptor } from '@angular/common/http'
 import {} from './authentication.service'
 
 @Injectable({
   providedIn: 'root'
 })
-export class BasicAuthHtppInterceptorServiceService {
+export class BasicAuthHtppInterceptorServiceService implements HttpInterceptor {
 
   constructor() { }
+
+  intercept(
+    req: import("@angular/common/http").HttpRequest<any>,
+    next: import("@angular/common/http").HttpHandler): import("rxjs").Observable<import("@angular/common/http").HttpEvent<any>> {
+      if (sessionStorage.getItem('username') && sessionStorage.getItem('basicauth')) {
+        req = req.clone({
+          setHeaders: {
+            Authorization: sessionStorage.getItem('basicauth')
+          }
+        })
+      }
+      return next.handle(req);
+  }
 }
