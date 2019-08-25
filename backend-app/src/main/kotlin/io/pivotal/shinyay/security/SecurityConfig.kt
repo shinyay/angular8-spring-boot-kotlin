856646fcf667688ab21fc1e050c296ab3b170c54
shinyay <shinya.com@gmail.com>
2019-08-25 11:27:21 +0900

    Add Http access authorization for Options methods from Angular Client which uses Prefight action

--- backend-app/src/main/kotlin/io/pivotal/shinyay/security/SecurityConfig.kt
+++ backend-app/src/main/kotlin/io/pivotal/shinyay/security/SecurityConfig.kt
@@ -1,8 +1,29 @@
 package io.pivotal.shinyay.security
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(httpSecurity: HttpSecurity) {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+    }
+
+    @Autowired
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        super.configure(auth)
+    }
 }
