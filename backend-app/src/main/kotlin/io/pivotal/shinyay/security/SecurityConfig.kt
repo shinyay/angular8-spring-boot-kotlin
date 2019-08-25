60c69928dc69b0af54d8ebb4ed014ca1dd177feb
shinyay <shinya.com@gmail.com>
2019-08-25 11:03:11 +0900

    Add WebSecurityConfigurerAdapter successor

---
+++ backend-app/src/main/kotlin/io/pivotal/shinyay/security/SecurityConfig.kt
@@ -0,0 +1,8 @@
+package io.pivotal.shinyay.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+}
