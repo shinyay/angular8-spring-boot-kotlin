e02271cd290226892bb53061331c92df2861b7fc
shinyay <shinya.com@gmail.com>
2019-08-25 20:00:24 +0900

    Add validate login credential access to backend service

--- frontend-app/src/app/service/authentication.service.ts
+++ frontend-app/src/app/service/authentication.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClientService } from './http-client.service';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment'
+
+export class User{
+  constructor(public status:string) {}
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   // [Hardcoded Credentials\
   // authenticate(username, password) {
@@ -19,6 +26,17 @@ export class AuthenticationService {
   //     return false;
   //   }
   // }
+  authenticate(username, password) {
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password)});
+    return this.httpClient.get<User>(environment.apiUrl + '/validate', {headers}).pipe(
+      map(
+        userData => {
+          sessionStorage.setItem('username', username);
+          return userData;
+        }
+      )
+    );
+  }
 
   isUserLoggedIn() {
     let user = sessionStorage.getItem('username')
