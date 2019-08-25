07464bbb1ef24c897807fabad21ba3d71899d51a
shinyay <shinya.com@gmail.com>
2019-08-25 10:25:19 +0900

    Add AuthGuardService to prevent from accessing somepages without login

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { EmployeeComponent} from './employee/employee.component';
 import { NewEmployeeComponent } from './new-employee/new-employee.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { AuthGuardService } from "./service/auth-guard.service";
 
 const routes: Routes = [
-  { path:'', component: EmployeeComponent},
-  { path:'newemployee', component: NewEmployeeComponent},
+  { path:'', component: EmployeeComponent, canActivate:[AuthGuardService]},
+  { path:'newemployee', component: NewEmployeeComponent, canActivate:[AuthGuardService]},
   { path:'login', component: LoginComponent },
-  { path: 'logout', component: LogoutComponent},
+  { path: 'logout', component: LogoutComponent, canActivate:[AuthGuardService]},
 
 ];
 
